ccd6469b9325ea328e7f3179d3bcf61c1422d3a7
Kainechukwu Duru <bismarckkaine@gmail.com>
2021-05-26 23:39:18 +0100

    working nodejs file uploader

--- APIs/imageUpload.js
+++ APIs/imageUpload.js
@@ -3,15 +3,39 @@ const router = express.Router();
 const multer = require("multer")
 const uuid = require("uuid").v4;
 
-const upload = multer({dest: "uploads/"});
+// const upload = multer({dest: "uploads/"});
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+let upload = multer({ storage: storage }).single('file')
 
 router.route("/")
-.post(upload.single("file"), function (req, res){
-    const {file} = req;
-    console.log("imageUploadFile: ", file);
-    
+    .post(function (req, res) {
+        // const {file} = req;
+        // console.log("imageUploadFile: ", file);
+        upload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                console.log("err:", err)
 
-    res.send("successful");
-});
+                return res.status(500).json(err)
+            } else if (err) {
+                console.log("err:", err)
+                return res.status(500).json(err)
+                
+
+            }
+            return res.status(200).send(req.file)
+
+        })
+
+        
+    });
 
 module.exports = router;
